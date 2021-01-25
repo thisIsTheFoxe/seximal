21c0ab5f179b29670c34454af7eb8ae2d9e3546a
Henrik Storch <thisisthefoxe@gmail.com>
2021-01-25 16:19:19 +0100

    Initial Commit

---
+++ seximal/seximalApp.swift
@@ -0,0 +1,17 @@
+//
+//  seximalApp.swift
+//  seximal
+//
+//  Created by Henrik Storch on 25.01.21.
+//
+
+import SwiftUI
+
+@main
+struct seximalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
