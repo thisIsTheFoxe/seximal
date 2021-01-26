3d4d817f1ab23292a797020e6d65c37c73f87c17
Henrik Storch <thisisthefoxe@gmail.com>
2021-01-26 20:50:47 +0100

    ADDED BASE CONVERER (FOR DOUBLE WITH FRACTIONAL COMPONENT)!!!!!

---
+++ seximal/LengthUnit.swift
@@ -0,0 +1,87 @@
+//
+//  LengthUnit.swift
+//  seximal
+//
+//  Created by Henrik Storch on 26.01.21.
+//
+
+import Foundation
+
+extension UnitLength {
+    static var sticks: UnitLength {
+        let converter = UnitConverterLinear(coefficient: 0.9572)
+        return UnitLength(symbol: NSLocalizedString("st", comment: "sticks"), converter: converter)
+    }
+    
+    static var niftistick: UnitLength {
+        let converter = UnitConverterLinear(coefficient: 0.9572 / 36)
+        return UnitLength(symbol: NSLocalizedString("nftst", comment: "niftistick"), converter: converter)
+    }
+    
+    static var untistick: UnitLength {
+        let converter = UnitConverterLinear(coefficient: 0.9572 / (36 * 36))
+        return UnitLength(symbol: NSLocalizedString("untst", comment: "untistick"), converter: converter)
+    }
+    
+    static var fetastick: UnitLength {
+        let converter = UnitConverterLinear(coefficient: 0.9572 * 36)
+        return UnitLength(symbol: NSLocalizedString("ftast", comment: "fetastick"), converter: converter)
+    }
+    
+    static var grandstick: UnitLength {
+        let converter = UnitConverterLinear(coefficient: 0.9572 * 36 * 36)
+        return UnitLength(symbol: NSLocalizedString("grdst", comment: "grandstick"), converter: converter)
+    }
+}
+
+extension Measurement where UnitType == UnitLength {
+    
+}
+
+extension MeasurementFormatter {
+    func customString(from unit: Unit) -> String {
+        guard self.unitStyle == .long else {
+            return self.string(from: unit)
+        }
+        
+        switch unit {
+        case UnitLength.sticks: return "sticks"
+        case UnitLength.niftistick: return "niftistick"
+        case UnitLength.untistick: return "untistick"
+        case UnitLength.fetastick: return "fetastick"
+        case UnitLength.grandstick: return "grandstick"
+            
+        default: return self.string(from: unit)
+        }
+    }
+}
+extension StringProtocol {
+    subscript(_ offset: Int)                     -> Element     { self[index(startIndex, offsetBy: offset)] }
+}
+
+extension Double {
+    public init?<S>(_ text: S, radix: Int = 10) where S : StringProtocol {
+        let comp = text.components(separatedBy: Locale.current.decimalSeparator ?? "")
+        guard comp.count <= 2, comp.count > 0 else {
+            return nil
+        }
+        
+        let intPart = comp[0]
+        guard let int = Int(intPart, radix: radix) else {
+            return nil
+        }
+        
+        var fractal = 0.0
+        if comp.count == 2 {
+            let fractalPart = comp[1]
+            for cIx in 0..<fractalPart.count {
+                let char = String(fractalPart[cIx])
+                guard let charAsInt = Int(char, radix: radix) else { return nil }
+                let bPart = Int(truncating: NSDecimalNumber(decimal: pow(Decimal(radix), cIx + 1)))
+                fractal += Double(charAsInt * (1 / bPart))
+            }
+        }
+        
+        self.init(Double(int) + fractal)
+    }
+}
