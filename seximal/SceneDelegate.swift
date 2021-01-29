835b25de5b4bc063ad79617d8d35488ff29eab36
Henrik Storch <thisisthefoxe@gmail.com>
2021-01-29 12:18:54 +0100

    fix macOS screenshots + appStore submit

---
+++ seximal/SceneDelegate.swift
@@ -0,0 +1,40 @@
+import UIKit
+import SwiftUI
+import os
+
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+        
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        connectingSceneSession.scene?.delegate = SceneDelegate()
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
+
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        guard let scene = (scene as? UIWindowScene) else { return }
+
+        #if targetEnvironment(macCatalyst)
+        let size = CGSize(width: 2560 / 2 - 112, height: 1600  / 2 - 112)
+        scene.sizeRestrictions?.minimumSize = size
+        scene.sizeRestrictions?.maximumSize = size
+        #endif
+    }
+}
+
+
+//1440x900
