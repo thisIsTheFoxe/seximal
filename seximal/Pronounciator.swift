f6d93821e2e3a0266e0699b7093255c13455c005
Henrik Storch <thisisthefoxe@gmail.com>
2021-04-02 10:33:20 +0200

    added big numbers up to pentexian (aka 10^32 aka DEC6^DEC20)

--- seximal/Pronounciator.swift
+++ seximal/Pronounciator.swift
@@ -15,6 +15,42 @@ extension Int {
       return formatter.string(from: numberValue) ?? ""
     }
     
+    /*
+    func spellAsExianInSex() -> String {
+        
+        let bi = self / 1296
+        let biRest = self / 1_679_616
+        let triRest = self % 1_679_616
+        let tri = self / 1_679_616
+
+        var result = ""
+        
+        switch self {
+        case 1..<1296:
+            if self > 1 {
+                result += self.spellInSex() + " "
+            }
+            result += "unexian"
+            return result
+        case 1296..<1_679_616:
+            if bi > 1 {
+                result += bi.spellInSex() + " "
+            }
+            result += "biexian" + biRest.spellAsExianInSex()
+            return result
+        case 1_679_616..<2_176_782_336:
+            if tri > 1 {
+                result += tri.spellInSex() + " "
+            }
+            result += "triexian" + biRest.spellAsExianInSex()
+            return result
+        default:
+            return ""
+        }
+        
+    }
+*/
+    
     func spellInSex() -> String {
         guard !(0...12).contains(self) else {
             if self == 0 { return "" }
@@ -24,15 +60,23 @@ extension Int {
         let dozens = self / 6
         let nifs = self / 36
         let nifRest = self % 36
-        let exian = self / 1296
-        let exianRest = self % 1296
+        let unexian = self / 1296
+        let unexianRest = self % 1296
+        let biexian = self / 1_679_616
+        let biexianRest = self % 1_679_616
+        let triexian = self / 2_176_782_336
+        let triexianRest = self % 2_176_782_336
+        let quadexian = self / 2_821_109_907_456
+        let quadexianRest = self % 2_821_109_907_456
         let rest = self % 6
+        
+        ///NOTE: Not switchng self, but divided by six
         switch dozens {
         case 2: return "dozen " + rest.spellInSex()
         case 3: return "thirsy " + rest.spellInSex()
         case 4: return "foursy " + rest.spellInSex()
         case 5: return "fifsy " + rest.spellInSex()
-        case 6...215:
+        case 6..<216:
             var result = ""
             if nifs > 1 {
                 result += nifs.spellInSex() + " "
@@ -42,16 +86,52 @@ extension Int {
                 result += " " + nifRest.spellInSex()
             }
             return result
-        case 216...279_935:
+        //6**(8-1)
+        case 216..<279_936:
             var result = ""
-            if exian > 1 {
-                result += exian.spellInSex() + " "
+            if unexian > 1 {
+                result += unexian.spellInSex() + " "
             }
             result += "unexian"
-            if exianRest > 0 {
-                result += " " + exianRest.spellInSex()
+            if unexianRest > 0 {
+                result += " " + unexianRest.spellInSex()
             }
             return result
+            //6**(12-1)
+        case 279_936..<362_797_056:
+            var result = ""
+            if biexian > 1 {
+                result += biexian.spellInSex() + " "
+            }
+            result += "biexian"
+            if biexianRest > 0 {
+                result += " " + biexianRest.spellInSex()
+            }
+            return result
+            //6**(16-1)
+        case 362_797_056..<470_184_984_576:
+            var result = ""
+            if triexian > 1 {
+                result += triexian.spellInSex() + " "
+            }
+            result += "triexian"
+            if triexianRest > 0 {
+                result += " " + triexianRest.spellInSex()
+            }
+            return result
+        //6**(20-1)
+        case 470_184_984_576..<609_359_740_010_496:
+            var result = ""
+            if quadexian > 1 {
+                result += quadexian.spellInSex() + " "
+            }
+            result += "quadexian"
+            if quadexianRest > 0 {
+                result += " " + quadexianRest.spellInSex()
+            }
+            return result
+        case 609_359_740_010_496:
+            return "pentexian"
         default:
             return ""
         }
